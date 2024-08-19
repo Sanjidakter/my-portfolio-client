19a08c2c8846e82f9c6e7a1d84b768fc3386363f
Sanjida Akter <sanjidakter2304@gmail.com>
2024-08-19 20:21:49 +0600

    solving error of home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-
-import { Routes, Route } from 'react-router-dom';
-import About from './components/About';
-import Contact from './components/Contact';
-import Blog from './components/Blog';
-import BlogDetails from './components/BlogDetails';
-import Layout from './components/Layout';
-import Home from './components/home';
-
+import { Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Blog from "./components/Blog";
+import BlogDetails from "./components/BlogDetails";
+import Home from "./components/Home";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}/>
-      <Route index element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/blog" element={<Blog />} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,25 @@
-
+import React from 'react';
 
 const Home = () => {
-  return (
-    <div className="container">
-      <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row">
-          <img
-            src="https://images.pexels.com/photos/7651559/pexels-photo-7651559.jpeg?auto=compress&cs=tinysrgb&w=600"
-            className="max-w-sm rounded-lg shadow-2xl"
-          />
-          <div>
-            <h1 className="text-5xl font-bold">My Portfolio!</h1>
-            <p className="py-6">
-              Know my day to day task here. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, consectetur vel, numquam ipsa necessitatibus laborum dignissimos neque architecto quaerat explicabo voluptates assumenda maxime pariatur magnam dicta vitae quos itaque inventore?
-            </p>
-            <button href="/blog" className="btn btn-primary">Get Started</button>
+    return (
+        <div className="container">
+        <div className="hero bg-base-200 min-h-screen">
+          <div className="hero-content flex-col lg:flex-row">
+            <img
+              src="https://images.pexels.com/photos/7651559/pexels-photo-7651559.jpeg?auto=compress&cs=tinysrgb&w=600"
+              className="max-w-sm rounded-lg shadow-2xl"
+            />
+            <div>
+              <h1 className="text-5xl font-bold">My Portfolio!</h1>
+              <p className="py-6">
+                Know my day to day task here. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, consectetur vel, numquam ipsa necessitatibus laborum dignissimos neque architecto quaerat explicabo voluptates assumenda maxime pariatur magnam dicta vitae quos itaque inventore?
+              </p>
+              <button href="/blog" className="btn btn-primary">Get Started</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
